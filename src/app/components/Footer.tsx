2308574fd08172a713602f1d532b47154836147b
Manohar Ramarao <manoharramarao@gmail.com>
2024-01-12 11:28:42 +0530

    created TODOs for home component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FooterComponent = () => {
+  return {
+    // TODO - create footer component
+  };
+};
+
+export default FooterComponent;
--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -5,11 +5,30 @@ import { useGlobalContext } from "../contexts/GlobalContext";
 import { Box, Container } from "@mui/material";
 
 const HomeComponent = () => {
+  // TODO - create state for carousle image URLs and footer content
+
+  // TODO - Create function to fetch carousel content from server
+
+  // TODO - Create function to fetch footer content from server
+
+  React.useEffect(() => {
+    // TODO - call above functions to get carousel and footer content
+  }, []);
+
   const { accessToken } = useGlobalContext();
   return (
     <React.Fragment>
       <Container>
-        {accessToken !== "" && <Box>User logged in! here is the access token ${accessToken}</Box>}
+        {accessToken !== "" && (
+          <React.Fragment>
+            {
+              // TODO - create Carousel component
+              // TODO - create contaner to main content
+              // TODO - Include footer component after creating separate footer component
+            }
+            <Box>User logged in! here is the access token ${accessToken}</Box>
+          </React.Fragment>
+        )}
         {accessToken === "" && <Box>User is not logged in.</Box>}
       </Container>
     </React.Fragment>
